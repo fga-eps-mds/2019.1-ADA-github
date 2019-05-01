b194e510df88d3b7554ea25446f979668acdeb8f
guilherme-mendes <guimendesp12@gmail.com>
2019-04-30 22:09:38 -0300

    #156 Initial commit for github requests

--- api/github/issue/utils.py
+++ api/github/issue/utils.py
@@ -1,5 +1,18 @@
 # api/github/__init__.py
 
+import requests
+from requests.exceptions import HTTPError
+import json
+
+
 class Issue():
-    pass
+    def __init__(self, GITHUB_API_TOKEN):
+        self.GITHUB_API_TOKEN = GITHUB_API_TOKEN
+
+    def get_list_issues(self, project_org, project_name):
+        response = requests.get(
+            "https://api.github.com/orgs/:org/issues")
+    def create_issue():
+        response = requests.get(
+            "https://api.github.com/orgs/:org/issues")
 
