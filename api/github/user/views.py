3f40668f810a1e82525495056ca07820163b2e96
guilherme-mendes <guimendesp12@gmail.com>
2019-05-08 16:53:08 -0300

    #183 User name and repository request for access token
    Co-authored-by: Lucas Fellipe <lucasfcm9@gmail.com>

--- api/github/user/views.py
+++ api/github/user/views.py
@@ -1,6 +1,8 @@
 from flask import jsonify, Blueprint, redirect, url_for, request
 from flask_cors import CORS
 from github.user.utils import User
+from github.user.error_messages import NOT_FOUND, UNAUTHORIZED
+from requests.exceptions import HTTPError
 import requests
 import sys
 import json
@@ -18,28 +20,40 @@ def ping_pong():
         "message": "pong!"
     }), 200
 
+
 @github_blueprint.route("/user/signin/callback", methods=["GET"])
 def index():
     code = request.args.get('code')
-    header = {"Content-Type": "application/json"}
-    
+    header = {"Accept": "application/json"}
+
     data = {
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET,
-            "code": code
-            }
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "code": code
+    }
     scope = "admin:repo_hook,repo"
-    a = "https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}".format(
+    oauth_user = "https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}".format(
         code=code, client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
     data = json.dumps(data)
-    post = requests.post(url=a,
+    post = requests.post(url=oauth_user,
                          headers=header)
-    # return token
-    post_str = str(post.content)
-    # post_str = post_str.split("=")[1]
-    # access_token = post_str.replace("&scope","")
-    print(post_str, file=sys.stderr)
-    #save to db
+    post_str = post.json()
+    GITHUB_TOKEN = post_str['access_token']
+    try:
+        user = User(GITHUB_TOKEN)
+        username = user.get_user()
+        requested_repos = user.get_repos()
+    except HTTPError as http_error:
+        dict_message = json.loads(str(http_error))
+        if dict_message["status_code"] == 401:
+            return jsonify(UNAUTHORIZED), 401
+        else:
+            return jsonify(NOT_FOUND), 404
+    except AttributeError:
+        return jsonify(NOT_FOUND), 404
+    else:
+        return jsonify(
+            username,
+            requested_repos
+        ), 200
 
-    return jsonify({"message": "success"})
-    # return access_token
