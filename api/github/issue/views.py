6ae8d4464122f88df93663e460dcd1cb672e737e
sudjoao <isudjoao@gmail.com>
2019-05-11 09:25:52 -0300

    #156 Use db data to create pipeline
    Co-authored-By: Caio Fernades <caiovf13@gmail.com>

--- api/github/issue/views.py
+++ api/github/issue/views.py
@@ -7,6 +7,8 @@ import requests
 from requests.exceptions import HTTPError
 import os
 from github.issue.utils import Issue
+from github.data.user import User
+from github.data.project import Project
 
 issue_blueprint = Blueprint("issue", __name__)
 CORS(issue_blueprint)
@@ -20,15 +22,20 @@ def ping_pong():
         "message": "pong!"
     }), 200
 
-@issue_blueprint.route("/api/new_issue/<repository_name>",
+@issue_blueprint.route("/api/new_issue/<chat_id>",
                        methods=["POST"])
-def create_issue(repository_name):
+def create_issue(chat_id):
     try:
-        r = request.get_json()
-        title = r['title']
-        body = r['body']
+        response = request.get_json()
+        title = response['title']
+        body = response['body']
+
+        user = User()
+        user = User.objects(chat_id=chat_id).first()
+        project = Project()
+        project = user.project   
         issue = Issue(GITHUB_API_TOKEN)
-        create_issue = issue.create_issue(repository_name, title,body)
+        create_issue = issue.create_issue(project.name, title, body)
     except HTTPError as http_error:
         dict_message = json.loads(str(http_error))
         if dict_message["status_code"] == 401:
