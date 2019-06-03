41bf585fdfe7b57815a5a9bb4d6ef46d18e5c095
Caio Fernandes <caiovf13@gmail.com>
2019-06-02 23:40:34 -0300

    #235 Add contributor_issues utils.py file

--- api/github/contributor_issues/utils.py
+++ api/github/contributor_issues/utils.py
@@ -2,12 +2,13 @@ import requests
 from requests.exceptions import HTTPError
 import json
 
+
 class ContributorIssues():
 
     def __init__(self, GITHUB_TOKEN):
         self.GITHUB_TOKEN = GITHUB_TOKEN
 
-    def get_contributor_issues(self, repo_fullname ,contributor_name):
+    def get_contributor_issues(self, repo_fullname, contributor_name):
         headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer " + self.GITHUB_TOKEN
@@ -18,22 +19,43 @@ class ContributorIssues():
                 "https://api.github.com/repos/{repo_fullname}"
                 "/issues?assignee={contributor_name}"
                 .format(repo_fullname=repo_fullname,
-                 contributor_name=contributor_name),
-                 headers=headers)
+                        contributor_name=contributor_name),
+                headers=headers)
             response.raise_for_status()
         except HTTPError as http_error:
             dict_error = {"status_code": http_error.response.status_code}
             raise HTTPError(json.dumps(dict_error))
         else:
-            issues_info = [{}]
+            issues_info = []
             repo_issues = response.json()
             for issue_info in repo_issues:
                 issues_info.append({
                     "title": issue_info["title"],
-                    "url": issue_info["url"],
+                    "url": issue_info["html_url"],
                     "issue_number": issue_info["number"]
                 })
             return issues_info
-            
 
+    def get_project_fullname(self, project_name):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self.GITHUB_TOKEN
+        }
+        try:
+            response = requests.get(
+                "https://api.github.com/user/repos",
+                headers=headers)
 
+            response.raise_for_status()
+        except HTTPError as http_error:
+            dict_error = {"status_code": http_error.response.status_code}
+            raise HTTPError(json.dumps(dict_error))
+
+        else:
+            repositories = response.json()
+            for item in repositories:
+                if(item["name"] == project_name):
+                    owner_and_repo = item["full_name"]
+                    break
+
+        return owner_and_repo
