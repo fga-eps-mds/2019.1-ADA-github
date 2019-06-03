bcf5d96e20d1ccd79c2b665c746cb6e67d20f6dd
Caio Fernandes <caiovf13@gmail.com>
2019-06-02 23:41:02 -0300

    #235 Add contributor_issues views.py file

--- api/github/contributor_issues/views.py
+++ api/github/contributor_issues/views.py
@@ -1,25 +1,31 @@
-from flask import jsonify, Blueprint, request
+from flask import jsonify, Blueprint
 from flask_cors import CORS
 from github.contributor_issues.utils import ContributorIssues
 import json
 from github.issue.error_messages import NOT_FOUND, UNAUTHORIZED
 from requests.exceptions import HTTPError
 from github.data.user import User
-from github.data.project import Project
 
 contributor_issues_blueprint = Blueprint("contributor_issues", __name__)
 CORS(contributor_issues_blueprint)
 
 @contributor_issues_blueprint.route("/api/get_contributor_issues/<chat_id>/"
-                        "<contributor_username>", methods=["GET"])
+                                    "<contributor_username>", methods=["GET"])
 def get_contributor_issues(chat_id, contributor_username):
-    user = User.objects(chat_id=chat_id).first()
-    project = user.project
-    repo_fullname = "fga-eps-mds/2019.1-ADA"
-    contributor_issues = ContributorIssues(user.access_token)
-    issues = contributor_issues.get_contributor_issues(repo_fullname, contributor_username)
-    return jsonify(issues)
-
-
-
-    
+    try:
+        user = User.objects(chat_id=chat_id).first()
+        project = user.project
+        contributor_issues = ContributorIssues(user.access_token)
+        repo_fullname = contributor_issues.get_project_fullname(project.name)
+        issues = contributor_issues.\
+            get_contributor_issues(repo_fullname, contributor_username)
+    except HTTPError as http_error:
+        dict_message = json.loads(str(http_error))
+        if dict_message["status_code"] == 401:
+            return jsonify(UNAUTHORIZED), 401
+        else:
+            return jsonify(NOT_FOUND), 404
+    except AttributeError:
+        return jsonify(NOT_FOUND), 404
+    else:
+        return jsonify(issues), 200
