b625adfb22d223e03534d1afe5b8ef9704410fb9
Caio Fernandes <caiovf13@gmail.com>
2019-06-04 08:26:16 -0300

    #135 Fix flake8 warnings

--- api/github/contributor_issues/views.py
+++ api/github/contributor_issues/views.py
@@ -5,7 +5,6 @@ import json
 from github.issue.error_messages import NOT_FOUND, UNAUTHORIZED
 from requests.exceptions import HTTPError
 from github.data.user import User
-import sys
 
 contributor_issues_blueprint = Blueprint("contributor_issues", __name__)
 CORS(contributor_issues_blueprint)
@@ -15,7 +14,6 @@ CORS(contributor_issues_blueprint)
                                     "<contributor_username>", methods=["GET"])
 def get_contributor_issues(chat_id, contributor_username):
     try:
-        print("###"*60 + "\n" + chat_id + "###"*60 + "\n", file=sys.stderr)
         user = User.objects(chat_id=chat_id).first()
         project = user.project
         contributor_issues = ContributorIssues(user.access_token)
