c1a288b2c460327f6817a2997d0d1f72a25a0e5d
sudjoao <isudjoao@gmail.com>
2019-06-04 15:38:14 -0300

    #211 Fix branches tests
    Co-authored-By: Lucas Fellipe <lucasfcm9@gmail.com>

--- api/github/tests/base.py
+++ api/github/tests/base.py
@@ -3,11 +3,28 @@
 from flask_testing import TestCase
 from github import create_app
 from github.data import init_db
+import os
+from github.data.user import User
+from github.data.project import Project
+import sys
 
 
 class BaseTestCase(TestCase):
     def setUp(self):
         self.db = init_db()
+        self.user = User()
+        self.user.username = 'sudjoao'
+        self.user.chat_id = '339847919'
+        self.user.github_user = 'sudjoao'
+        self.user.github_user_id = '46005310'
+        self.user.access_token = os.getenv("GITHUB_API_TOKEN", "")
+        self.project = Project()
+        self.project.name = 'apitest'
+        self.project_id = '185474581'
+        self.project.save()
+        self.user.project = self.project
+        self.user.save()
+        self.GITHUB_API_TOKEN = os.getenv("GITHUB_API_TOKEN", "")
 
     def create_app(self):
         app = create_app()
