3e2cb8dfebed6c8701118070724644d0e6365427
Bruno Dantas <oliveiradantas96@gmail.com>
2019-06-17 00:17:33 -0300

    #262 Fix unit test of the test_user.py.
    Co-authored-by: Caio Fernandes <caiovf13@gmail.com>

--- api/github/user/utils.py
+++ api/github/user/utils.py
@@ -17,6 +17,9 @@ ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "")
 class UserInfo(GitHubUtils):
     def __init__(self, chat_id):
         super().__init__(chat_id)
+        self.headers = {
+            "Content-Type": "application/json"
+        }
 
     def get_own_user_data(self):
         url = self.GITHUB_API_URL + "user?access_token="\
@@ -28,7 +31,10 @@ class UserInfo(GitHubUtils):
         return github_data
 
     def get_repositories(self):
-        url = self.GITHUB_API_URL + "user/repos"
+        requested_username = self.get_own_user_data()
+        username = requested_username["github_username"]
+        url = self.GITHUB_API_URL + "users/{login}/repos".format(
+                                     login=username)
         requested_repositories = self.request_url(url, "get")
         project_repositories = self.repository_requested_repository(
                                     requested_repositories)
